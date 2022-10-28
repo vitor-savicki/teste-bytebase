90b665fb0125366bf180370fc45bd5b4d3411126
Vitor Savicki <vitorsavicki@gmail.com>
2022-10-28 16:22:20 -0300

    [Bytebase] Update latest schema for "blog" after migration 202208171630
    THIS COMMIT IS AUTO-GENERATED BY BYTEBASE
    
    https://ad19-2804-d55-585f-d200-4681-6884-fb0c-e7ac.ngrok.io/issue/blog-alter-schema-108?stage=109
    
    --------Original migration change--------
    
    https://github.com/vitor-savicki/teste-bytebase/commit/8ea43faaf7b9384f99c61ecac61ebf37c2828dad
    
    Create blog__202208171630__migrate__add_table.sql

--- bytebase/Test/.blog##LATEST.sql
+++ bytebase/Test/.blog##LATEST.sql
@@ -29,6 +29,21 @@ CREATE TABLE `post` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Table structure for `teste`
+--
+CREATE TABLE `teste` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `content` text COLLATE utf8mb4_general_ci NOT NULL,
+  `post_id` int NOT NULL,
+  `author_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `post_id` (`post_id`),
+  KEY `author_id` (`author_id`),
+  CONSTRAINT `teste_ibfk_1` FOREIGN KEY (`post_id`) REFERENCES `post` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `teste_ibfk_2` FOREIGN KEY (`author_id`) REFERENCES `user` (`id`) ON DELETE RESTRICT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
 -- Table structure for `user`
 --
 CREATE TABLE `user` (
